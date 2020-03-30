af8c47329b08cfa7f373e910536331f4536043d9
Fabrizio Montesi <famontesi@gmail.com>
2020-03-30 15:10:07 +0200

    Week 14.

---
+++ exercises/src/cp/week14/ThreadsExercise12.java
@@ -0,0 +1,12 @@
+package cp.week14;
+
+/**
+ *
+ * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
+ */
+public class ThreadsExercise12
+{
+	/*
+	Modify Threads/cp/WalkBlockingDeque such that only files ending with a ".txt" suffix are put in the tasks deque.
+	*/
+}
---
+++ exercises/src/cp/week14/ThreadsExercise13.java
@@ -0,0 +1,13 @@
+package cp.week14;
+
+/**
+ *
+ * @author Fabrizio Montesi <fmontesi@imada.sdu.dk>
+ */
+public class ThreadsExercise13
+{
+	/*
+	Modify Threads/cp/WalkExecutor such that all threads stop processing as soon as the word "nulla" is found.
+	Hint: Use an atomic boolean shared by all threads. Try not to make it a static field, but rather a local variable within the main method.
+	*/
+}
