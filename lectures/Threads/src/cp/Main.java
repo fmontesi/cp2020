39f6710ec331cbe4146f0f872226f7455ca3230d
Fabrizio Montesi <famontesi@gmail.com>
2020-04-20 13:48:36 +0200

    Weeks 17 and 18.

--- lectures/Threads/src/cp/Main.java
+++ lectures/Threads/src/cp/Main.java
@@ -6,6 +6,17 @@ public class Main
 	{
 //		Utils.doAndMeasure( Walk::main );
 //		Utils.doAndMeasure( WalkBlockingDeque::main );
-		Utils.doAndMeasure( WalkExecutor::main );
+//		Utils.doAndMeasure( WalkExecutor::main );
+//		Utils.doAndMeasure( WalkExecutorFuture::main );
+//		Utils.doAndMeasure( WalkCompletionService::main );
+//		Utils.doAndMeasure( WalkCompletableFuture::main );
+//		Utils.doAndMeasure( WalkSequentialStream::main );
+//		Utils.doAndMeasure( WalkParallelStream1::main );
+//		Utils.doAndMeasure( WalkParallelStream2::main );
+//		Utils.doAndMeasure( WalkParallelStream3::main );
+//		Utils.doAndMeasure( WalkParallelStream4::main );
+//		Utils.doAndMeasure( WalkParallelStream5::main );
+//		Utils.doAndMeasure( WalkParallelStreamFindAny::main );
+//		Utils.doAndMeasure( WalkCompletionServiceFindAny::main );
 	}
 }
